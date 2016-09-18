9b535d3b7105a1cb4160b36e8c78cdb732d08c10
Mallea Paulline <malleapaulline@Malleas-MacBook-Pro.local>
2016-09-17 23:01:50 -0400

    Updated footnote hover

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+
+	// footnote function
 	
+	$( ".clickme" ).click(function() {
+		  $( ".footnote" ).toggle(function() {
+		    // Animation complete.
+		  }); 
+
+		});
+	$( "dropdown-menu" ).hooverfunction() {
+		  $( ".show" ).show(function() {
+		    // Animation complete.
+		  }); 
+
+	// end of footnote function   ------------------------------------------------
 });
